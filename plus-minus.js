b01d93e04e57c35956b99b5b54940b9e0d95ea36
Saraha <sah110_@outlook.com>
2024-12-11 22:02:00 -0500

    Update plus-minus.js

--- plus-minus.js
+++ plus-minus.js
@@ -1,3 +1,5 @@
+// Given an array of integers, calculate the ratios of its elements that are positive, negative, and zero. Print the decimal value of each fraction on a new line with  places after the decimal. Print the ratios of positive, negative and zero values in the array. Each value should be printed on a separate line with  digits after the decimal. The function should not return a value.
+
 function plusMinus(arr) {
     let positiveCount = 0
     let negativeCount = 0
